a96fb57996c92c78f9f7d8f60be17bf20a2a9db5
Mike Fuller <mike.fuller@fishtownanalytics.com>
2023-02-15 17:08:52 +0000

    add type 6 example

---
+++ models/demo_examples/slowly_changing_dimensions/dim_bookings_scd6.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized = 'table'
+    )
+}}
+
+select id,
+status,
+state,
+country,
+{{ cents_to_dollars('booking_amt',2) }} as booking_usd,
+event_dt,
+dbt_scd_id,
+dbt_updated_at,
+dbt_valid_from,
+dbt_valid_to,
+{{ scd2_cols('id', 'event_dt') }},
+{{ scd3_cols('status,state', 'id', 'event_dt') }} 
+from {{ ref('bookings_snapshot') }}
+order by id, event_dt
