4532266e2ad7d6c6a4fb21629665856f689a2e10
Mike Fuller <mike.fuller@fishtownanalytics.com>
2023-08-24 14:58:58 +0000

    fix syntax

--- macros/sample_run_op_query_tag.sql
+++ macros/sample_run_op_query_tag.sql
@@ -1,5 +1,5 @@
-% macro sample_run_op_query_tag() %}
-  {% set query_tag = "test_a_tag"() %}
-  {% do run_query("alter session set query_tag = '{}'".format(query_tag)) %}
+{% macro sample_run_op_query_tag() %}
+  {% set sample_query_tag = "test_a_tag" %}
+  {% do run_query("alter session set query_tag = '{}'".format(sample_query_tag)) %}
   {% do run_query("select current_date()") %}
 {% endmacro %}
