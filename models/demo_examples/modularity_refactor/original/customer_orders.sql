d41c08846bd0480881897b5720b805234d0dcd7e
Mike Fuller <mike.fuller@fishtownanalytics.com>
2024-02-15 23:26:00 +0000

    add fully qualified names

--- models/demo_examples/modularity_refactor/original/customer_orders.sql
+++ models/demo_examples/modularity_refactor/original/customer_orders.sql
@@ -6,20 +6,20 @@ WITH paid_orders as (select Orders.ID as order_id,
     p.payment_finalized_date,
     C.FIRST_NAME    as customer_first_name,
         C.LAST_NAME as customer_last_name
-FROM jaffle_orders as Orders
+FROM mike_fuller_sandbox.refactored_jaffle.jaffle_orders as Orders
 left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-        from jaffle_payments
+        from mike_fuller_sandbox.refactored_jaffle.jaffle_payments
         where STATUS <> 'fail'
         group by 1) p ON orders.ID = p.order_id
-left join jaffle_customers C on orders.USER_ID = C.ID ),
+left join mike_fuller_sandbox.refactored_jaffle.jaffle_customers C on orders.USER_ID = C.ID ),
 
 customer_orders 
 as (select C.ID as customer_id
     , min(ORDER_DATE) as first_order_date
     , max(ORDER_DATE) as most_recent_order_date
     , count(ORDERS.ID) AS number_of_orders
-from jaffle_customers C 
-left join jaffle_orders as Orders
+from mike_fuller_sandbox.refactored_jaffle.jaffle_customers C 
+left join mike_fuller_sandbox.refactored_jaffle.jaffle_orders as Orders
 on orders.USER_ID = C.ID 
 group by 1)
 
