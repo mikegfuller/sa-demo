c9af343b6e146fb386eae8bb618ec0ed3d109ebe
Mike Fuller <mike.fuller@fishtownanalytics.com>
2024-02-16 02:37:22 +0000

    fix issue with order by

--- macros/scd2_cols.sql
+++ macros/scd2_cols.sql
@@ -2,8 +2,8 @@
 
    {%- set scd2_logic -%}
         {{date_field}} as eff_from
-        ,case when lead({{key_field}}) over(order by {{key_field}}) = {{key_field}} then lead({{date_field}}) over(order by {{key_field}}) else null end as eff_to
-        ,case when lead({{key_field}}) over(order by {{key_field}}) = {{key_field}} then 'N' else 'Y' end as current_flg      
+        ,case when lead({{key_field}}) over(order by {{key_field}},{{date_field}}) = {{key_field}} then lead({{date_field}}) over(order by {{key_field}}) else null end as eff_to
+        ,case when lead({{key_field}}) over(order by {{key_field}},{{date_field}}) = {{key_field}} then 'N' else 'Y' end as current_flg      
     {%- endset -%}
 
 {{ return(scd2_logic) }}
